3f94d005ec4b3489d563d4b490f8691fb4227b4f
Arne Küderle <a.kuederle@gmail.com>
2019-04-07 11:04:04 +0200

    Added missing import

--- helper/height_from_baro.py
+++ helper/height_from_baro.py
@@ -1,10 +1,13 @@
+import numpy as np
+
+
 def height_from_baro(baro, ref_altitudes=None):
     altitude = 44330 * (1.0 - (baro / 1013.0) ** 0.1903)
     if ref_altitudes:
         alt_diffs = list()
         for lift in ref_altitudes:
             alt_diffs.append(ref_altitudes[lift][0] - altitude[int(ref_altitudes[lift][1])])
-        
+
         altitude = altitude + np.mean(alt_diffs)
-    
+
     return altitude
